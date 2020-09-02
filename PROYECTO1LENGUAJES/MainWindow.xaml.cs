bb81248354345221fec1f47bdc2e582fafd9e990
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-09-01 19:05:57 -0600

    Diseno de interfaz grafica

--- PROYECTO1LENGUAJES/MainWindow.xaml.cs
+++ PROYECTO1LENGUAJES/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PROYECTO1LENGUAJES
         {
             InitializeComponent();
         }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
     }
 }
