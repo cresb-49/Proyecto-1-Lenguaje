7ad900c3a071ed73cd22e609000b5a4eb16a6307
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-09-02 03:48:56 -0600

    Solucion de comportamientos y apertura de archivos

--- PROYECTO1LENGUAJES/MainWindow.xaml.cs
+++ PROYECTO1LENGUAJES/MainWindow.xaml.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace PROYECTO1LENGUAJES
 {
@@ -20,6 +21,8 @@ namespace PROYECTO1LENGUAJES
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Boolean archivoCargado =false;
+        private String src="";
         public MainWindow()
         {
             InitializeComponent();
@@ -35,19 +38,69 @@ namespace PROYECTO1LENGUAJES
         {
             this.Close();
         }
-        
+
         private void propiedadesGraficas()
         {
-            Color colorBarra = (Color)ColorConverter.ConvertFromString("#061826");
-            Color colorContenedorPrincipal = (Color)ColorConverter.ConvertFromString("#30343F");
-            Color colorCamposDeTexto = (Color)ColorConverter.ConvertFromString("#364652");
-            BarraID.Background = new SolidColorBrush(colorBarra);
-            contenedorPrincipal.Background = new SolidColorBrush(colorContenedorPrincipal);
-            tituloApartadoCompilador.Background = new SolidColorBrush(colorBarra);
-            CampoDeEscritura.Background = new SolidColorBrush(colorCamposDeTexto);
-            CompilerLog.Background = new SolidColorBrush(colorCamposDeTexto);
             CampoDeEscritura.Document.PageWidth = 2000;
+            this.MenuItemGuardar.IsEnabled = false;
+            this.CampoDeEscritura.IsEnabled = false;
         }
 
+        private void ButtonMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            CampoDeEscritura.Document.Blocks.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Source code (*.gt)|*.gt";
+            if (openFileDialog.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                src = openFileDialog.FileName;
+                String textoLeido = File.ReadAllText(openFileDialog.FileName);
+                CampoDeEscritura.AppendText(textoLeido);
+                archivoCargado = true;
+                this.CampoDeEscritura.IsEnabled = true;
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (archivoCargado == true)
+            { 
+                File.Delete(src);
+                StreamWriter escritura = new StreamWriter(src);
+                TextRange rango = new TextRange(CampoDeEscritura.Document.ContentStart, CampoDeEscritura.Document.ContentEnd);
+                String linea = rango.Text;
+                Console.WriteLine(linea);
+                escritura.WriteLine(linea);
+                escritura.Close();
+            }
+            else
+            {
+                SaveFileDialog guardado = new SaveFileDialog();
+                guardado.Filter = "Source code(*.gt) | *.gt";
+                guardado.Title = "Guardar Sorce Code";
+                guardado.FileName = "Sin titulo 1";
+                var resultado = guardado.ShowDialog();
+                if (resultado == System.Windows.Forms.DialogResult.OK)
+                {
+                    //StreamWriter escritura = new StreamWriter(guardado.FileName);
+                }
+            }
+            
+        }
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
