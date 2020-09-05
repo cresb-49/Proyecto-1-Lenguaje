46180293c10fdcf9362dfef0ba676900d53e57ae
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-09-05 02:52:45 -0600

    Clasificacion de tokens

---
+++ PROYECTO1LENGUAJES/ElemetosDeLengua/ComponentesLinguisticos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO1LENGUAJES.ElemetosDeLengua
+{
+    class ComponentesLinguisticos
+    {
+        private String[] datosPrimitivos = new String[] { "entero", "decimal", "cadena", "booleano", "caracter" };
+        private String[] operadoresAritmeticos = new String[] { "+", "-", "*", "/", "++", "--" };
+        private String[] operadoresRelacionales = new String[] { ">", "<", ">=", "<=", "==", "!=" };
+        private String[] operadoresLogicos = new String[] { "||", "&&", "!" };
+        private String[] signosAgrupacion = new String[] { "(", ")" };
+        private String asignacionDeSentencia = "=";
+        private String finalizacionSentencia = ";";
+        private String[] palabrasRecervadas = new String[] { "SI", "SINO", "SINO_SI", "MENTRAS", "HACER", "DESDE", "HASTA", "INCREMENTO" };
+        private String[] comentarios = new String[] {"//","/*","*/"};
+        //Constructor vacio de la clase de componentes linguisticos
+        public ComponentesLinguisticos()
+        {
+
+        }
+        public Boolean AnalicisPalabrasRecervadas(String cadenaAnalizar)
+        {
+            for(int i = 0; i < palabrasRecervadas.Length; i++)
+            {
+                if (cadenaAnalizar.Equals(palabrasRecervadas.GetValue(i)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Boolean AnilicisTiposPrimitivos(String cadenaAnalizar)
+        {
+            for (int i = 0; i < palabrasRecervadas.Length; i++)
+            {
+                if (cadenaAnalizar.Equals(datosPrimitivos.GetValue(i)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
---
+++ PROYECTO1LENGUAJES/ElemetosDeLengua/SeparadorDeTexto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO1LENGUAJES.ElemetosDeLengua
+{
+    class SeparadorDeTexto
+    {
+        //Constructor vacio de la separacion de texto
+        public SeparadorDeTexto()
+        {
+
+        }
+        
+        public String cadenaReortada(String lineaAProcesar)
+        {
+            String respuesta="";
+
+            return respuesta;
+        }
+        public List<String> lineasTexto(String textoAnalizar)
+        {
+            List<String> resultados = new List<String>();
+
+            return resultados;
+        }
+    }
+}
